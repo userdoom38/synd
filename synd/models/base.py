9dc7e7bc60de8fcbd3a1c149f9b61e80eacd68a2
willeagren <wilhelmagren98@gmail.com>
2023-05-11 22:03:44 +0200

    Model loading and rng state setting

--- synd/models/base.py
+++ synd/models/base.py
@@ -22,21 +22,74 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 File created: 2023-05-10
-Last updated: 2023-05-10
+Last updated: 2023-05-11
 """
+
+from __future__ import annotations
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from synd.typing import *
 
 class Synthesizer(object):
     """ Base synthesizer class for all synthetic data generator models. """
+    def __init__(self, *args, **kwargs):
+        self.set_rng_state(**kwargs)
 
     def fit(self, *args, **kwargs):
         raise NotImplementedError(
-            f'No training method is implemented for {self.__class__}. ',
-            f'Please implement a training process for synthesizer.',
+            f'No training method is implemented for {self.__class__}. '
+            f'Please implement a training process for synthesizer.'
         )
 
     def sample(self, *args, **kwargs):
         raise NotImplementedError(
-            f'No sampling method is implemented for {self.__class__}. ',
-            f'Please implement a sampling process for the synthesizer.',
+            f'No sampling method is implemented for {self.__class__}. '
+            f'Please implement a sampling process for the synthesizer.'
         )
+
+    def to(self, device: Union[String, torch.device]) -> Synthesizer:
+        """ Move all `torch.nn` attributes of `self` to the specified `device`. """
+        for attr in self.__dict__.values():
+            if isinstance(attr, nn.Module):
+                attr.to(device)
+
+        return self
+
+    @classmethod
+    def from_pretrained(cls, path: String) -> Synthesizer:
+        model = torch.load(path)
+        device = torch.device(
+            'cuda:0' if torch.cuda.is_available() else 'cpu',
+        )
+
+        model.to(device)
+        return model
+
+    def set_rng_state(self,
+        state: Union[Integer, Tuple[np.random.RandomState, torch.Generator]] = 1337,
+        **kwargs: Dict,
+    ):
+        """ Set the state for all relevant random number generators. """
+        # see below link for random number generation management
+        # https://github.com/sdv-dev/CTGAN/blob/master/ctgan/synthesizers/base.py
+
+        if isinstance(state, int):
+            self._rng_state = (
+                np.random.RandomState(seed=state),
+                torch.Generator().manual_seed(state),
+            )
+        elif (
+            isinstance(state, tuple) and
+            isinstance(state[0], np.random.RandomState) and
+            isinstance(state[1], torch.Generator)
+        ):
+            self._rng_state = state
+        else:
+            raise TypeError(
+                f'The provided state should either be `int` or (`np.random.RandomState`, '
+                f'`torch.Generator`). Function got: {state}.'
+            )
 
